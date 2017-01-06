799740d487e80d3b68d4f5165e241a374d8545d9
MADISPC\Madis <Madis.Heinsoo@gmail.com>
2017-01-06 06:55:01 +0200

    Revert "Removed unused imports and dead code. Few extra comments in HomePage"
    This reverts commit 9773a547b97c6471c1317832be213208cbb26aab.

--- src/main/java/com/schoolstuff/HomePage.java
+++ src/main/java/com/schoolstuff/HomePage.java
@@ -1,5 +1,11 @@
 package com.schoolstuff;
 
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
@@ -21,25 +27,17 @@ public class HomePage extends WebPage
         Document respXML = null;
         try
         {
-            //Creates connection object and streams information from constructed URL to response variable.
             URLConnection connection = new URL(url + "?" + query).openConnection();
             connection.setRequestProperty("Accept-Charset", charset);
             InputStream response = connection.getInputStream();
 
-            //Defines a factory API to create DOM object trees from XML document
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
 
-            //Parses information from recieved GET request into previously created Document object.
             respXML = builder.parse(response);
         }
-        catch(Exception e)
-        {
-            //There could possibly be 3 different exceptions in try block.
-            e.printStackTrace();
-        }
+        catch(Exception e){}
 
-        //Removes whitespaces and fixes some structure issues.
         respXML.getDocumentElement().normalize();
         return respXML;
     }
@@ -47,24 +45,43 @@ public class HomePage extends WebPage
 	public HomePage(final PageParameters parameters)
 	{
 		super(parameters);
+        final String url = "https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/v1/";
+        final String charset = StandardCharsets.UTF_8.toString();
+        final String key = "DD2F5A5CC5099227EA909DE0C6B7E35D";
+        final String matchID = "2880899735";
+        final String format = "XML";
 
-		//Variables that are required for basic functioning.
-        String url = "https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/v1/";
-        String charset = StandardCharsets.UTF_8.toString();
-        String key = "DD2F5A5CC5099227EA909DE0C6B7E35D";
-        String matchID = "2880899735";
-        String format = "XML";
-
-        //Generates query part in URL
         String query = String.format("format=%s&match_id=%s&key=%s", format, matchID, key);
 
-        //Calls responseXML method and saves return value in Document object
-        Document response = responseXML(url, charset, query);
+        final MatchidModel matchidModel = new MatchidModel();
 
-        //Creates a match object.
+        Form<?> form = new Form("messageInputForm");
+
+        final TextField<String> text = new TextField<String>("text", new PropertyModel<String>(matchidModel, "matchid"));
+
+        Button button = new Button("submit")
+        {
+            @Override
+            public void onSubmit()
+            {
+                String query = String.format("format=%s&match_id=%s&key=%s", format, text.getConvertedInput(), key);
+                Document response = responseXML(url, charset, query);
+                MatchDetails match = new MatchDetails(response);
+                add(new Label("matchnumber", match.getMatchid()));
+                add(new Label("matchduration", match.getDuration()));
+                add(new Label("rscore", match.getRadiantScore()));
+                add(new Label("dscore", match.getDireScore()));
+                add(new Label("winner", match.getWinner()));
+                super.onSubmit();
+            }
+        };
+        add(form);
+        form.add(text);
+        form.add(button);
+
+        Document response = responseXML(url, charset, query);
         MatchDetails match = new MatchDetails(response);
 
-        //Shows information on html file
         add(new Label("matchnumber", match.getMatchid()));
         add(new Label("matchduration", match.getDuration()));
         add(new Label("rscore", match.getRadiantScore()));
